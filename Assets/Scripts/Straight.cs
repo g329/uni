190e1543aea8be1d08c0bee6a34142d640ee50e4
_329_ <329g329@gmail.com>
2016-10-08 15:21:24 +0900

    対向車Prefab追加

---
+++ Assets/Scripts/Straight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace MLPlayer {		
+	public class Straight : MonoBehaviour {
+
+		public float speed;
+		[SerializeField] GameObject Mob;
+
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+				Mob.transform.Translate (new Vector3 (speed, 0, 0));
+		}
+
+		void OnEvent(GameObject other) {
+			
+		}
+
+		void OnTriggerEnter(Collider other) {
+		}
+
+		void OnCollisionEnter(Collision collision) {
+
+		}
+
+	}
+}
